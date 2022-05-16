83bf2a7d9316857e0836afc244ae308401d084e3
Nabig Chaudhry <nabig@lbl.gov>
2022-05-16 12:11:15 -0700

    cleaning function code

--- masters_project_functions.py
+++ masters_project_functions.py
@@ -116,9 +116,6 @@ def bootstrap(ams, data_variable="return_value", arg_value=10):
             return_value = fitted_distr.ppf(return_event)
             result = round(return_value, 5)
 
-        elif data_variable == "return_prob":
-            result = 1-(fitted_distr.cdf(arg_value))
-
     except ValueError:
         result = np.nan
     
@@ -222,59 +219,3 @@ def get_flat_array(ds, data_variable='return_value', remove_nans=True):
         flat_array = flat_array[~np.isnan(flat_array)]
     
     return flat_array
-
-#####################################################################
-# GET RETURN PROBABILITY
-# input: annual maximum series and relevant parameters
-# export: xarray dataset with return probabilities and confidence intervals
-
-def get_return_prob(ams, threshold, bootstrap_runs=1000,
-                    conf_int_lower_bound=2.5, conf_int_upper_bound=97.5):
-
-    ams_attributes = ams.attrs
-    ams = ams.stack(allpoints=["x", "y"]).squeeze().groupby("allpoints")
-
-    def return_prob(ams):
-
-        try:
-            lmoments = ldistr.gev.lmom_fit(ams)
-            fitted_distr = stats.genextreme(**lmoments)
-            return_prob = 1 - (fitted_distr.cdf(threshold))
-        except ValueError:
-            return_prob = np.nan
-            
-        bootstrap_values = []
-        data_variable = "return_prob"
-        for _ in range(bootstrap_runs):
-            result = bootstrap(ams, data_variable="return_prob", arg_value=threshold)
-            bootstrap_values.append(result)
-        conf_int_array = np.percentile(bootstrap_values, [conf_int_lower_bound,
-                                                          conf_int_upper_bound])
-        conf_int = tuple(conf_int_array)
-
-        return return_prob, conf_int
-
-    return_prob, conf_int = xr.apply_ufunc(
-                            return_prob,
-                            ams,
-                            input_core_dims=[["time"]],
-                            exclude_dims=set(("time",)),
-                            output_core_dims=[[],[]]
-    )
-
-    return_prob = return_prob.rename("return_prob")
-    new_ds = return_prob.to_dataset()
-    new_ds["conf_int"] = conf_int
-
-    new_ds = new_ds.unstack("allpoints")
-
-    new_ds["return_prob"].attrs["threshold"] = "exceedance of {} value event".format(
-        str(threshold)
-    )
-    new_ds["conf_int"].attrs["confidence interval bounds"] = "({}th percentile, {}th percentile)".format(str(conf_int_lower_bound), str(conf_int_upper_bound))
-    new_ds.attrs = ams_attributes
-    new_ds.attrs["distribution"] = "gev"
-
-    return new_ds
-
-#####################################################################
